41323f53cacd45de8e9c0ef25574cf12f41d1107
ingJuanArenas <juanarenasing@gmail.com>
2026-01-06 10:45:28 -0500

    mapping

---
+++ src/main/java/com/courses/persistence/mapper/StudentsMapper.java
@@ -0,0 +1,22 @@
+package com.courses.persistence.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import com.courses.domain.dtos.StudentDTO;
+import com.courses.persistence.model.Student;
+
+@Mapper(componentModel = "spring")
+public interface StudentsMapper {
+    
+    @Mapping(target = "id", ignore = true)
+    Student toEntity (StudentDTO studentDTO);
+
+    StudentDTO toDto (Student student);
+    List<StudentDTO> toDtos (List<Student> students);
+
+    void UpdateEntityFromDto(StudentDTO courseDTO, @MappingTarget Student student);
+}
