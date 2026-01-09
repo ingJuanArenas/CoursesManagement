0e97770aeb59a5d9ba8babdd9da904c6b535739a
ingJuanArenas <juanarenasing@gmail.com>
2026-01-09 11:44:30 -0500

    updating project: new  characteristics.

--- src/main/java/com/courses/persistence/model/Course.java
+++ src/main/java/com/courses/persistence/model/Course.java
@@ -1,12 +1,13 @@
 package com.courses.persistence.model;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -33,8 +34,15 @@ public class Course {
 
     private String description;
 
-    @OneToMany(mappedBy = "course", fetch = FetchType.EAGER, orphanRemoval = true)
-    private List<Student> students = new ArrayList<>();
+    @Column(nullable = false)
+    private int capacity;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+   
+
+    @OneToMany(mappedBy = "course")
+    private List<Enrollment> enrollments = new ArrayList<>();
 
 
     
