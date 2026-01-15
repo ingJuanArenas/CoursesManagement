1c1f2d28514f521d482f90b2b1de7c5fe08bb6ff
ingJuanArenas <juanarenasing@gmail.com>
2026-01-15 10:17:54 -0500

    changing exception name and updating delete enrollment function

--- src/main/java/com/courses/domain/exceptions/EnrollmentNotAvaliableException.java
+++
@@ -1,7 +0,0 @@
-package com.courses.domain.exceptions;
-
-public class EnrollmentNotAvaliableException extends RuntimeException {
-    public EnrollmentNotAvaliableException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/courses/domain/exceptions/EnrollmentOperationNotAvaliableException.java
@@ -0,0 +1,7 @@
+package com.courses.domain.exceptions;
+
+public class EnrollmentOperationNotAvaliableException extends RuntimeException {
+    public EnrollmentOperationNotAvaliableException(String message) {
+        super(message);
+    }
+}
