b186504ccb7cda91656f2a785360aa69d3c2a196
ingJuanArenas <juanarenasing@gmail.com>
2026-02-06 12:03:02 -0500

    Using JpaRepository only

--- src/main/java/com/courses/persistence/crud/EnrollmentsCRUD.java
+++ src/main/java/com/courses/persistence/crud/EnrollmentsCRUD.java
@@ -1,8 +1,32 @@
 package com.courses.persistence.crud;
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
+import com.courses.domain.dtos.EnrollmentDTO;
 import com.courses.persistence.model.Enrollment;
+import com.courses.persistence.model.EnrollmentStatus;
+import com.courses.persistence.projections.CoursesSummary;
+import com.courses.persistence.projections.StudentsSummary;
 
-public interface EnrollmentsCRUD extends ListCrudRepository<Enrollment,Long> {
-    
+public interface EnrollmentsCRUD extends JpaRepository<Enrollment,Long> {
+     @Query("SELECT DISTINCT c FROM Course c JOIN FETCH Enrollment e ON c.id = e.courseId WHERE e.studentId = :id")
+    Page<CoursesSummary> findCoursesByStudentId(Long id,Pageable pageable);
+
+    @Query("SELECT DISTINCT s FROM Student s JOIN FETCH Enrollment e ON s.id= e.studentId WHERE e.courseId = :id")
+    Page<StudentsSummary> findStudentsByCourseId(Long id, Pageable pageable);
+
+    Page<Enrollment> findAllByEnrollmentDate(LocalDate date, Pageable pageable);
+
+    Page<Enrollment> findByStatus(EnrollmentStatus status, Pageable pageable);
+
+    Page<Enrollment> findByCourseId(Long courseId, Pageable pageable);
+
+    Page<Enrollment> findByStudentId(Long studentId, Pageable pageable);
+
+  
 }
--- src/main/java/com/courses/persistence/crud/StudentsCRUD.java
+++ src/main/java/com/courses/persistence/crud/StudentsCRUD.java
@@ -1,9 +1,45 @@
 package com.courses.persistence.crud;
 
 
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.courses.persistence.model.Student;
+import com.courses.persistence.projections.StudentsSummary;
 
-public interface StudentsCRUD extends ListCrudRepository<Student,Long>{
+public interface StudentsCRUD extends JpaRepository<Student,Long>{
+
+
+    @Query(
+        value = """
+            select s.name as name, s.email as email
+            from Student s
+            where s.active = true
+            
+        """,
+        countQuery = """
+            select count(s)
+            from Student s
+            where s.active = true
+        """
+    )
+    Page<StudentsSummary> findAllByActiveTrue(Pageable pageable);
+
+        @Query(
+        value = """
+            select s.name as name, s.email as email
+            from Student s
+            where s.active = true
+            and LOWER(s.name) like LOWER(CONCAT('%', :name, '%'))
+        """,
+        countQuery = """
+            select count(s)
+            from Student s
+            where s.active = true
+        """
+    )
+    Page<StudentsSummary> findAllByNameContainingIgnoreCaseAndActiveTrue(String name, Pageable pageable);
 }
