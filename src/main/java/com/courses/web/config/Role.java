8cd449923173721fb52ef9638c33954484eaf5be
ingJuanArenas <juanarenasing@gmail.com>
2026-02-17 11:12:10 -0500

    implementing spring security

---
+++ src/main/java/com/courses/web/config/Role.java
@@ -0,0 +1,23 @@
+package com.courses.web.config;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name; // ROLE_ADMIN, ROLE_STUDENT
+}
