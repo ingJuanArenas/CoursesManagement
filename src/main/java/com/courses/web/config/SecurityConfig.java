b90a06cf2a11fe95c5ae10e015a59bdba9ec2a52
ingJuanArenas <juanarenasing@gmail.com>
2026-03-02 11:16:18 -0500

    Applying JWT filter

--- src/main/java/com/courses/web/config/SecurityConfig.java
+++ src/main/java/com/courses/web/config/SecurityConfig.java
@@ -10,11 +10,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
      SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -24,8 +30,8 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth->{
             auth.requestMatchers("/api/auth/**").permitAll();
             auth.anyRequest().authenticated();
-        });
-
+        })
+        .addFilterBefore( jwtFilter , UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
